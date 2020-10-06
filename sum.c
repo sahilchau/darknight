afe77574cc0697c89323607797830f87c385dd6b
sahilchau <72430086+sahilchau@users.noreply.github.com>
2020-10-06 10:41:49 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conioh>
+void main()
+{
+    int a,b,s
+    printf("\nENTER THE VALUE OF A=")
+    printf("\nENTER THE VALUE OF B=")
+    scanf("%d%d",&a,&b)
+    s=a+b
+    printf("sum=%d",s)
+    getch()
+
+
+}
